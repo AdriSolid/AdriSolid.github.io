bba8ce4b5d1f15c8cd0cc2605b2590a3968e79e3
AdriSolid <biotekgis@gmail.com>
2019-05-26 20:06:02 +0200

    Add Slides App

---
+++ SlidesGirona2019/precache-manifest.530dff5ad61f17ec97f984a8473dc18c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ec9c2bcc9532ed7887b3913b7ef0e4",
+    "url": "./static/media/MVT_tags.d1ec9c2b.png"
+  },
+  {
+    "revision": "b6dd07b1225ad7f55fb21fcdc2a174b2",
+    "url": "./static/media/Gutenberg.b6dd07b1.png"
+  },
+  {
+    "revision": "2bd4bf871b27ee5a426874902c16ef21",
+    "url": "./static/media/GPU.2bd4bf87.png"
+  },
+  {
+    "revision": "aef33d87af9dc2b54ac6a8170e12cf9c",
+    "url": "./static/media/CPU.aef33d87.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "./static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "865cc337646dd1517b53",
+    "url": "./static/js/main.7323180c.chunk.js"
+  },
+  {
+    "revision": "95b990e4046135f8fd01",
+    "url": "./static/js/2.58f4849d.chunk.js"
+  },
+  {
+    "revision": "865cc337646dd1517b53",
+    "url": "./static/css/main.bfc4c3fa.chunk.css"
+  },
+  {
+    "revision": "95b990e4046135f8fd01",
+    "url": "./static/css/2.b24498f1.chunk.css"
+  },
+  {
+    "revision": "80f143dfdbac5dca4d592522f6b31f62",
+    "url": "/index.html"
+  }
+];
